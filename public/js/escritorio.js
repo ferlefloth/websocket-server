e319bd38844c943eaef5ed24ab79f74505bb7842
Fernando Lefloth <mellolefloth@gmail.com>
2023-07-02 00:27:31 -0300

    added logic for escritorio

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,48 @@
+//Referencias HTML
+const lblEscritorio = document.querySelector('h1')
+const btnAtender = document.querySelector('button')
+//const lblEscritorio = document.querySelector('h1')
 
 
-console.log('Escritorio HTML');
+
+const searchParams = new URLSearchParams( window.location.search ); //los params que viajan en la URL
+
+if( !searchParams.has('escritorio')){
+    console.log('adentro')
+    window.location ='index.html'
+    throw new Error('El escritorio es obligatorio')
+}
+
+// let escritorio2 =searchParams.get("escritorio") 
+// let escritorio = escritorio2;
+const escritorio = searchParams.get("escritorio") 
+//lblEscritorio.innerText = escritorio
+
+
+const socket = io();
+
+socket.on('connect', () => {
+    // console.log('Conectado');
+    btnAtender.disabled = false;
+
+
+});
+
+socket.on('disconnect', () => {
+    // console.log('Desconectado del servidor');
+
+    btnAtender.disabled = true;
+});
+
+
+socket.on('ultimo-ticket',(ultimoTicket)=>{
+  //  lblNuevoTicket.innerText = `Ticket ${ultimoTicket}`;
+})
+
+btnAtender.addEventListener( 'click', () => {
+
+    // socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+    //      lblNuevoTicket.innerText = ticket;
+    // });
+
+});
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,20 +1,20 @@
-const TicketContol = require('../models/ticket-control')
+const TicketControl = require('../models/ticket-control')
 
-const ticketContol = new TicketContol();
+const ticketControl = new TicketControl()
 
 
 const socketController = (socket) =>{ 
-    console.log('Cliente comnectado ', socket.id); // cada socket tiene un ID
+    socket.emit('ultimo-ticket', ticketControl.ultimo)
 
     socket.on('disconnect', ()=>{ // el SOCKET.ON es cuando el cliente lo envía y yo lo escucho
     //    console.log('cliente desconectado ',socket.id);
     })
 
-    socket.on('enviar-mensaje',(payload, callback)=>{ // Acá escuchamos desde el back , lo que está en el scoket-client en los add event listeners. El payload es el mensaje que viene desde la función del addListener del socket-cliente
-    
-        const id = 12345678
-        callback(id) // esto retroalimenta y le devuleve al cliente el id del mensaje que el envió
-        socket.broadcast.emit('enviar-mensaje', payload) //broadcast hace que le mande un mensaje a todos
+    socket.on('siguiente-ticket',(payload, callback)=>{ // Acá escuchamos desde el back , lo que está en el scoket-client en los add event listeners. El payload es el mensaje que viene desde la función del addListener del socket-cliente
+        const siguiente = ticketControl.siguiente();
+        callback(siguiente)
+
+        //TODO: Notificar que hay un ticket pendiente par aasignar
     })
 
 
@@ -25,3 +25,18 @@ const socketController = (socket) =>{
 module.exports = {
     socketController
 }
+
+/*
+
+    console.log('Cliente comnectado ', socket.id); // cada socket tiene un ID
+
+
+socket.on('enviar-mensaje',(payload, callback)=>{ // Acá escuchamos desde el back , lo que está en el scoket-client en los add event listeners. El payload es el mensaje que viene desde la función del addListener del socket-cliente
+    
+        const id = 12345678
+        callback(id) // esto retroalimenta y le devuleve al cliente el id del mensaje que el envió
+        socket.broadcast.emit('enviar-mensaje', payload) //broadcast hace que le mande un mensaje a todos
+    })
+
+
+*/
